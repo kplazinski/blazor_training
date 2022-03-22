b9a04070d06fe2fc1f2d351a5cbad7bd263bf27b
Konrad Plazinski <kplazinski@ecovadis.com>
2022-03-22 14:30:50 +0100

    init

---
+++ BlazorTraining/Model/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorTraining.Model
+{
+    public class Student
+    {
+        public Student(int id, string name, string lastName, int age, bool isActive)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Age = age;
+            IsActive = isActive;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
