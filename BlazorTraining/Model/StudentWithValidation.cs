4c8ed3e47256ab3c3a068a499df5fddd19289765
Konrad Plazinski <kplazinski@ecovadis.com>
2022-03-23 15:14:30 +0100

    form + basic fixes

---
+++ BlazorTraining/Model/StudentWithValidation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTraining.Model
+{
+    public class StudentWithValidation
+    {
+        public StudentWithValidation()
+        {
+
+        }
+        public StudentWithValidation(int id, string name, string lastName, int age, bool isActive)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Age = age;
+            IsActive = isActive;
+        }
+
+        [Required(ErrorMessage = "Id is required")]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Name is too short")]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Last name is too short")]
+        public string LastName { get; set; }
+
+        [Required]
+        public Sex Sex { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Age should be between 1 and 100")]
+        public int Age { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public enum Sex
+    {
+        Male, Female
+    }
+}
