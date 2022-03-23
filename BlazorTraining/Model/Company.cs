646c3e13b914f74a1e74b39ed2aa60ad902b0f98
Konrad Plazinski <kplazinski@ecovadis.com>
2022-03-23 10:48:30 +0100

    template component

---
+++ BlazorTraining/Model/Company.cs
@@ -0,0 +1,30 @@
+﻿namespace BlazorTraining.Model
+{
+    public class Company
+    {
+        public Company(int id, string companyName, CompanyAddress companyAddress)
+        {
+            Id = id;
+            CompanyName = companyName;
+            CompanyAddress = companyAddress;
+        }
+
+        public int Id { get; set;}
+        public string CompanyName { get; set; }
+        public CompanyAddress CompanyAddress { get; set; }
+    }
+
+    public class CompanyAddress
+    {
+        public CompanyAddress(string street, string city, string country)
+        {
+            Street = street;
+            City = city;
+            Country = country;
+        }
+
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
