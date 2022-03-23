26801187366ed5ccc748ca7c5cfdd1c9cfd5bd5f
Konrad Plazinski <kplazinski@ecovadis.com>
2022-03-23 12:47:29 +0100

    js

---
+++ BlazorTraining/Extensions/JSRuntimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorTraining.Extensions
+{
+    public static class JSRuntimeExtensions
+    {
+        public static async Task SwalAdvanced(this IJSRuntime jSRuntime,
+               string icon, string title, string text, string footerMsg, string footerLink)
+        {
+            string footer = $"<a href='{footerLink}'>{footerMsg}</a>";
+
+            await jSRuntime.InvokeVoidAsync("Swal.fire", new { icon, title, text, footer });
+        }
+    }
+}
